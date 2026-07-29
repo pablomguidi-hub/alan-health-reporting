2bc7904b7f28686645cceb0c4ccab659f37fae42
pablomguidi-hub <pablomguidi@gmail.com>
2026-07-29 19:34:54 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ Original file is located at
     https://colab.research.google.com/drive/1MtBB5Bsw3gJrdo0YIqM9ddSMNEKda_uV
 """
 
-!pip install streamlit pandas plotly
+
 import streamlit as st
 import pandas as pd
 import numpy as np
